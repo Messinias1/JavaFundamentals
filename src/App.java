366ab05c8153f32641b376ba5ad4bfaade4ba3f7
Carl Kakisis <Messinias1@gmail.com>
2020-03-05 10:32:09 -0500

    mini calculator

--- src/App.java
+++ src/App.java
@@ -5,5 +5,9 @@ public class App {
 
         Scanner keyboardInput = new Scanner(System.in);
         System.out.println("Enter in First Number: ");
+        double num1 = keyboardInput.nextDouble();
+        System.out.println("Enter Second Number: ");
+        double num2 = keyboardInput.nextDouble();
+        System.out.println(num1 + num2);
     }
 }
