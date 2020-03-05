af1b392dd338e9746049416540110333df85e30c
Carl Kakisis <Messinias1@gmail.com>
2020-03-04 23:57:59 -0500

    input

--- src/App.java
+++ src/App.java
@@ -4,11 +4,6 @@ public class App {
     public static void main(String[] args) {
 
         Scanner keyboardInput = new Scanner(System.in);
-        System.out.print("Enter Your Name: ");
-        String name = keyboardInput.nextLine();
-        System.out.print("Enter Your Age: ");
-        int age = keyboardInput.nextInt();
-        System.out.println("Hey " + name + " you are " +
-                age + " years old.");
+        System.out.println("Enter in First Number: ");
     }
 }
