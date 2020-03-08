af56166976a55b8d4fac91ab8bfa4531803b7133
Carl Kakisis <Messinias1@gmail.com>
2020-03-07 23:56:40 -0500

    playing with getters an setters

--- src/App.java
+++ src/App.java
@@ -1,10 +1,25 @@
 class Car {
     public String make;
     public String model;
+    private String vin;
 
     public Car(String aMake, String aModel) {
         this.make = aMake;
         this.model = aModel;
+    }
+
+    public void setVin(String vin) {
+        if(vin.length() == 17) {
+            this.vin = vin;
+        }
+        else {
+            System.out.println("Not a valid VIN");
+        }
+    }
+
+    public String getVin()
+    {
+        return vin;
     }
 }
 
@@ -12,6 +27,8 @@ public class App {
     public static void main(String[] args) {
 
         Car car1 = new Car("Saab", "9-3");
+        car1.setVin("aaaaaaaaaaaaaaaaa");
+        System.out.println(car1.getVin());
         System.out.println(car1.make);
         sayHi("Carl", 25);
     }
