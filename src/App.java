df62aa3410656775715aeb340771e0ffd868b9c3
Carl Kakisis <Messinias1@gmail.com>
2020-03-10 01:36:19 -0400

    built a guessing game

--- src/App.java
+++ src/App.java
@@ -1,10 +1,31 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) {
 
-        int i = 1;
-        while(i <= 10) {
-            System.out.println(i);
-            i++;
+        Scanner input = new Scanner(System.in);
+
+        String secretWord = "giraffe";
+        String guess = "";
+        int guessCount = 0;
+        int guessLimit = 3;
+        boolean outOfGuesses = false;
+
+        while(!guess.equals(secretWord) && !outOfGuesses) {
+            if(guessCount < guessLimit) {
+                System.out.println("Enter a guess: ");
+                guess = input.nextLine();
+                guessCount++;
+            }
+            else {
+                outOfGuesses = true;
+            }
+        }
+        if(outOfGuesses) {
+            System.out.println("You lose");
+        }
+        else {
+            System.out.println("You win");
         }
     }
 }
