59e80bfcf8a75051817e01baa0960705d5eb7c8e
Carl Kakisis <Messinias1@gmail.com>
2020-03-10 01:21:45 -0400

    while loops

--- src/App.java
+++ src/App.java
@@ -1,15 +1,10 @@
 public class App {
     public static void main(String[] args) {
-            boolean isMale = true;
-            boolean isTall = true;
 
-            if(isMale && isTall) {
-                System.out.println("You are male and tall");
-            } else if(isMale && !isTall) {
-                System.out.println("you are male but not tall");
-            }
-            else {
-                System.out.println("You are not male");
-            }
+        int i = 1;
+        while(i <= 10) {
+            System.out.println(i);
+            i++;
+        }
     }
 }
