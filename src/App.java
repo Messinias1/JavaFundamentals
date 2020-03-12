0756494894f621542e139c3a3adc100e35e0a0a4
Carl Kakisis <Messinias1@gmail.com>
2020-03-11 23:48:46 -0400

    2d array

--- src/App.java
+++ src/App.java
@@ -3,29 +3,11 @@ import java.util.Scanner;
 public class App {
     public static void main(String[] args) {
 
-        Scanner input = new Scanner(System.in);
-
-        String secretWord = "giraffe";
-        String guess = "";
-        int guessCount = 0;
-        int guessLimit = 3;
-        boolean outOfGuesses = false;
-
-        while(!guess.equals(secretWord) && !outOfGuesses) {
-            if(guessCount < guessLimit) {
-                System.out.println("Enter a guess: ");
-                guess = input.nextLine();
-                guessCount++;
-            }
-            else {
-                outOfGuesses = true;
-            }
-        }
-        if(outOfGuesses) {
-            System.out.println("You lose");
-        }
-        else {
-            System.out.println("You win");
-        }
+        int [][] numberGrid = {
+                {1, 2, 3},
+                {4, 5, 6},
+                {7, 8, 9},
+                {0}
+        };
     }
 }
