d360d8129101ba0994b6387623fa855adc3688ba
Carl Kakisis <Messinias1@gmail.com>
2020-03-19 00:12:20 -0400

    solution

--- src/App.java
+++ src/App.java
@@ -3,21 +3,36 @@ import java.util.*;
 import java.text.*;
 import java.math.*;
 import java.util.regex.*;
-class Person {
-    public String name;
-    public String surName;
-    public int age;
-
-    public Person(String aName, String aSurName, int aAge) {
-        this.name = aName;
-        this.surName = aSurName;
-        this.age = aAge;
-    }
-}
 
 public class App {
+
     public static void main(String[] args) {
-        Person person1 = new Person("Carl", "Kakisis", 25);
-        System.out.println(person1.name);
+        int i = 4;
+        double d = 4.0;
+        String s = "HackerRank ";
+
+        Scanner scan = new Scanner(System.in);
+
+        /* Declare second integer, double, and String variables. */
+        int i2;
+        double d2;
+        String s2;
+
+        /* Read and save an integer, double, and String to your variables.*/
+        i2 = scan.nextInt();
+        d2 = scan.nextDouble();
+        s2 = scan.nextLine();
+        s2 = scan.nextLine();
+        // Note: If you have trouble reading the entire String, please go back and review the Tutorial closely.
+
+        /* Print the sum of both integer variables on a new line. */
+        System.out.println(i + i2);
+        /* Print the sum of the double variables on a new line. */
+        System.out.println(d + d2);
+        /* Concatenate and print the String variables on a new line;
+        	the 's' variable above should be printed first. */
+        System.out.println(s + s2);
+
+        scan.close();
     }
 }
